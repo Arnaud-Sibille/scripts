202e894c9c9b8bd7b44394ddc1c8d8b1ee3b7df8
Arnaud Sibille <arsi@odoo.com>
2026-04-21 15:38:46 +0200

    some changes I guess

--- src/find_word.py
+++ src/find_word.py
@@ -1,51 +1,22 @@
 #!/usr/bin/python3
 
 import argparse
-import subprocess
 
-from utils import get_versionned_odoo_repos
-
+from .find_in_repos import find_in_repos
 
 ARGUMENTS = {
-    ("word", ): {
-        "help": "word to look for",
+    ("search_pattern", ): {
+        "help": "regex pattern",
     },
 }
-
-
-def find_word(word, extra_args=[]):
-    search_pattern = r'\b' + word + r'\b'
-    addons_path = get_versionned_odoo_repos()
-    for addon_path in addons_path:
-        grep_command = [
-            'grep',
-            '-n',
-            '-r',
-            '-E',
-            '--include',
-            '*.py',
-            '--include',
-            '*.js',
-            '--include',
-            '*.xml',
-            '--include',
-            '*.css',
-            '--include',
-            '*.scss',
-            search_pattern,
-            addon_path,
-        ]
-        command = grep_command + extra_args
-        print(command)
-        subprocess.run(command)
-
 
 def main():
     parser = argparse.ArgumentParser()
     for key, value in ARGUMENTS.items():
         parser.add_argument(*key, **value)
     args, extra_args = parser.parse_known_args()
-    find_word(args.word, extra_args=extra_args)
+    search_pattern = r'\b' + args.word + r'\b'
+    find_in_repos(search_pattern, extra_args=extra_args)
 
 if __name__ == "__main__":
     main()
--- src/git_pull_rebase.sh
+++ src/git_pull_rebase.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+set -x
 
 git switch $1 && git pull && git switch - && git rebase $1
